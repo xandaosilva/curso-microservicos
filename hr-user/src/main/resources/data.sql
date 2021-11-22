d72e3097ce379876ef9f7e867e35f87d52bfe13a
Alexandre Rogério <xandaosilva93@hotmail.com>
2021-11-22 11:47:54 -0300

    hr-user Postgres dev profile

--- hr-user/src/main/resources/data.sql
+++ hr-user/src/main/resources/data.sql
@@ -7,3 +7,10 @@ INSERT INTO tb_role (role_name) VALUES ('ROLE_ADMIN');
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
+
+
+
+
+
+
